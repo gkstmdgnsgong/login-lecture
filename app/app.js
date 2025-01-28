d66586dfe8d9be55e33fc9ac4605571bdc86f8bb
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-01-29 01:10:54 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); // 어떤 엔진으로 해석할 것인지
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
 module.exports = app;
