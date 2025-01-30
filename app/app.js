03fbaebe12b4fcecf0c987ea38eb0bbb42c055f6
gkstmdgnsgong <gkstmdgnsgong@gmail.com>
2025-01-30 17:16:33 +0900

    UserStorage 모델 구현

--- app/app.js
+++ app/app.js
@@ -19,5 +19,5 @@ app.use(bodyParser.json());
 //URL을 통해 전달되는 데이터에 한글 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
+app.use("/", home); // use -> 미들웨어를 등록해주는 메서드 !!!미들웨어의 순서는 중요, 위에서부터 순차적으로
 module.exports = app;
---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const { reduce } = require("async");
+
+class UserStorage{
+    static #users = {
+        id: ["woorimIt" ,"나개발" ,"김팀장"],
+        psword: ["1234", "1234", "123456"],
+        name : ["우리밋", "나개발", "김팀장"]
+    };
+
+    static getUsers(...fields){
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
